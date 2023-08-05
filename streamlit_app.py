fe80b132d2dd0b355b10df6d00dfff71be163465
Khaled El-Wafi <122227782+dea60349@users.noreply.github.com>
2023-08-05 17:12:37 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -58,14 +58,14 @@ import snowflake.connector
 [snowflake]
 user = "khelwafi"
 password = "Jamahiriya_1#"
-account = "te27853.westeurope.azure_westeurope"
+account = "te27853.west-europe.azure"
 warehouse = "pc_rivery_wh" 
 database = "pc_rivery_db" 
 schema = "public"
 role = "pc_rivery_role"
 
 
-
+# account = "te27853.westeurope.azure_westeurope"
 
 # my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 # my_cur = my_cnx.cursor()
